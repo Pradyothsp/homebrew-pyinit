6cb9279413c6fac92970baf2fadae420722afefa
GitHub Action <action@github.com>
2025-08-02 15:37:53 +0000

    Update formula to v0.0.3

--- Formula/pyinit.rb
+++ Formula/pyinit.rb
@@ -1,23 +1,23 @@
 class Pyinit < Formula
   desc "Interactive Python project scaffolding tool"
   homepage "https://github.com/Pradyothsp/pyinit"
-  version "0.0.02"
+  version "0.0.3"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Pradyothsp/pyinit/releases/download/v0.0.02/pyinit-darwin-amd64"
-      sha256 "dfdeb6d66d10cbd04b3b38ea57adbcddaf36be311d7e5e716d8a958e0320540b"
+      url "https://github.com/Pradyothsp/pyinit/releases/download/v0.0.3/pyinit-darwin-amd64"
+      sha256 "7097cd171ee81a44ea8e616066a1743eaf689eac1e9754f4415a8b5780f2b94e"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/Pradyothsp/pyinit/releases/download/v0.0.02/pyinit-darwin-arm64"
-      sha256 "093d98e425e6188276dc7c4ea56c2ed105b9632fb3c9212023e185ec837b0c32"
+      url "https://github.com/Pradyothsp/pyinit/releases/download/v0.0.3/pyinit-darwin-arm64"
+      sha256 "fc433c0ad5c32abbc208cc16c6271bced835a3a0aba8237776514d5d0d5b5dab"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Pradyothsp/pyinit/releases/download/v0.0.02/pyinit-linux-amd64"
-      sha256 "1a85bacbd9af86a10e75217226ea6257d19cbda52f86de49380a93f7d7410645"
+      url "https://github.com/Pradyothsp/pyinit/releases/download/v0.0.3/pyinit-linux-amd64"
+      sha256 "ad5995ce99a0ed7e243f388c94a0e7a1754f88c17652f4bb51abb3a07c862a78"
     end
   end
 
